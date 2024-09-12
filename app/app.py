389578260972dde56d13cbb90367f7fa0bd0a96d
Robert Emerencia <robert.emerencia@gmail.com>
2024-09-12 21:49:23 +0200

    Remove uploaded_file from app.py

--- app/app.py
+++ app/app.py
@@ -59,9 +59,5 @@ def process_csv(filepath):
 
     return graph_filename
 
-#@app.route('/static/uploads/<filename>')
-#def uploaded_file(filename):
-#    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
 if __name__ == "__main__":
     app.run(debug=True)
